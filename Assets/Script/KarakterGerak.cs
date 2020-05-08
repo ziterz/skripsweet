79dd28ae34e7b9d8ffe7969321452333a64b4b16
Azizah Nurul K <azizahnurulk@gmail.com>
2020-05-08 16:29:06 +0700

    karakter movement

---
+++ Assets/Script/KarakterGerak.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarakterGerak : MonoBehaviour
+{
+
+    public int kecepatan,kekuatanLompat;
+    
+    // balik badan
+    public bool balik;
+    public int pindah;
+
+    // Rigidbody2D lompat;
+
+    // public bool tanah;
+	// public LayerMask targetlayer;
+	// public Transform deteksitanah;
+
+    Animator anim;
+
+    public bool tombolkiri, tombolkanan;
+    
+    void Start()
+    {
+        // lompat = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.D)|| (tombolkanan == true)) {
+			anim.SetBool ("gerak", true);
+			transform.Translate (Vector2.right * kecepatan * Time.deltaTime);
+			pindah = 1;
+		} else if (Input.GetKey (KeyCode.A) || (tombolkiri == true)) {
+			anim.SetBool ("gerak", true);
+			transform.Translate (Vector2.right * -kecepatan * Time.deltaTime);
+			pindah = -1;
+		}else{
+            anim.SetBool ("gerak", false);
+        }
+
+        // if(Input.GetKey (KeyCode.W)){
+        //     lompat.AddForce(new Vector2(0,kekuatanLompat));
+        // }
+
+        if (pindah > 0 && !balik) {
+			balikbadan();
+		}else if(pindah<0 && balik){
+			balikbadan();
+		}
+
+    }
+
+    void balikbadan(){
+		balik = !balik;
+		Vector3 karakter = transform.localScale;
+		karakter.x *= -1;
+		transform.localScale = karakter;
+	}
+
+    public void klikiri(){
+        tombolkiri = true;
+    }
+
+    public void lepaskiri(){
+        tombolkiri = false;
+    }
+
+    public void klikanan(){
+        tombolkanan = true;
+    }
+
+    public void lepaskanan(){
+        tombolkanan = false;
+    }
+}
---
+++ Assets/Script/camerafollow2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow2 : MonoBehaviour {
+    public GameObject player;
+    public float jarak;
+    
+    // Update is called once per frame
+    void Update() {
+            Vector3 pos = new Vector3(player.transform.localPosition.x + (player.transform.localScale.x * jarak), 0, -10f);
+            transform.localPosition = Vector3.Slerp(transform.localPosition, pos, 0.03f);
+    }
+}
