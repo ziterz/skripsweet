1084225cb51c4f650b9c113ef1ec9632db62500d
Azizah Nurul K <azizahnurulk@gmail.com>
2020-06-02 14:49:40 +0700

    sedikit lagi

---
+++ Assets/Script/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameOver : MonoBehaviour
+{
+
+    //public GameObject toolbar;
+    public TMP_Text pretest, posttest;
+
+    public void OnFinish()
+    {
+        //toolbar.gameObject.SetActive(false);
+        pretest.text = PlayerPrefs.GetInt("Pretest").ToString();
+        posttest.text = PlayerPrefs.GetInt("Posttest").ToString();
+       // winnerName.text = PlayerPrefs.GetString("Username");
+    }
+}
--- Assets/Script/SetUsername.cs
+++ Assets/Script/SetUsername.cs
@@ -6,7 +6,6 @@ public class SetUsername : MonoBehaviour
     public TMP_InputField nameField;
 
     public GameObject homePanel, namePanel;
-    public GameObject kasus, tutor, mulai;
     
     public void OnSubmit()
     {
@@ -14,13 +13,12 @@ public class SetUsername : MonoBehaviour
         PlayerPrefs.SetString("Username",nameField.text);
         PlayerPrefs.SetInt("Position", 0); 
         PlayerPrefs.SetInt("Level", 0);
-        PlayerPrefs.SetInt("Intro", 1);
+        //PlayerPrefs.SetInt("Intro", 1);
         PlayerPrefs.SetInt("ShowPretest", 1);
         PlayerPrefs.SetInt("Pretest", 0);
+        PlayerPrefs.SetInt("PretestDone", 0);
+        PlayerPrefs.SetInt("KuisionerDone", 0);
         PlayerPrefs.SetInt("Postest", 0);
-        // PlayerPrefs.SetInt("KuisDDL", 0);
-        // PlayerPrefs.SetInt("KuisDML", 0);
-        // PlayerPrefs.SetInt("KuisERD", 0);
         PlayerPrefs.SetInt("nilaiKuis", 0);
         PlayerPrefs.SetInt("mainLevel", 0);
         PlayerPrefs.SetInt("LevelERD", 0);
@@ -36,32 +34,21 @@ public class SetUsername : MonoBehaviour
     {
         homePanel = GameObject.Find("HomePanel");
         namePanel = GameObject.Find("NamePanel");
-        kasus.SetActive(false);
-        tutor.SetActive(false);
-        mulai.SetActive(true);
 
         if(PlayerPrefs.GetInt("ShowPretest") == 1)
         {
             namePanel.gameObject.SetActive(false);
             homePanel.gameObject.SetActive(true);
-            PlayerPrefs.SetInt("ShowPretest", 0);
-
-            if(PlayerPrefs.GetString("Username") != null){
-                if(PlayerPrefs.GetString("GayaBelajar") == "StudiKasus"){
-                    kasus.SetActive(true);
-                    tutor.SetActive(false);
-                    mulai.SetActive(false);
-                } else if(PlayerPrefs.GetString("GayaBelajar") == "Tutorial"){
-                    tutor.SetActive(true);
-                    kasus.SetActive(false);
-                    mulai.SetActive(false); 
-                }
-            }
+           // PlayerPrefs.SetInt("ShowPretest", 0);
         }
         else
         {
             namePanel.gameObject.SetActive(true);
             homePanel.gameObject.SetActive(false);
         }
+    }
+
+    public void Reset(){
+        PlayerPrefs.SetInt("ShowPretest", 1);
     }
 }
